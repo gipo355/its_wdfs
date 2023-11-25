f5aaefde51d2e553a5799669638c54a7fc725f46
gipo355 <91525512+gipo355@users.noreply.github.com>
2023-11-25 12:07:07 +0100

    WIP - add monorepo

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,13 @@
 module.exports = {
   extends: ['@gipo355/eslint-config-base'],
-    settings: {
+  settings: {
     'import/resolver': {
       typescript: {
         alwaysTryTypes: true,
-        // project: './tsconfig.json',
         project: './tsconfig.eslint.json',
-        // project: [
-        //     './tsconfig.json',
-        //     './tsconfig.eslint.json',
-        // ],
       },
     },
   },
-
   parserOptions: {
     tsconfigRootDir: __dirname, // new addition
     project: ['./tsconfig.eslint.json'],
