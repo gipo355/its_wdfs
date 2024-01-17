2c6dd93a8a67ffdbb8b0abf0339f279c073b77c5
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-01-17 15:04:56 +0100

    WIP add to solution

---
+++ csharp/20240117/Program20240117Controller/WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace Program20240117Controller;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
