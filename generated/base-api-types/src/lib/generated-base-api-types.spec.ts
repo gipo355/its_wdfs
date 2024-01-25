9fc8067cb6adb5e49a72c777f750a402cb5bdfab
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-01-25 14:41:43 +0100

    remove generated

--- generated/base-api-types/src/lib/generated-base-api-types.spec.ts
+++
@@ -1,7 +0,0 @@
-import { generatedBaseApiTypes } from './generated-base-api-types';
-
-describe('generatedBaseApiTypes', () => {
-  it('should work', () => {
-    expect(generatedBaseApiTypes()).toEqual('generated-base-api-types');
-  });
-});
--- generated/base-api-types/src/lib/generated-base-api-types.ts
+++
@@ -1,3 +0,0 @@
-export function generatedBaseApiTypes(): string {
-  return 'generated-base-api-types';
-}
